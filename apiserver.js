e262515da689096a29445dfc45b39be8442341d3
seriousme <hans.klunder@xs4all.nl>
2016-07-03 19:38:38 +0200

    Reworked sync request handling
    Sync requests are now completed by using the change feed from the
    database

--- apiserver.js
+++ apiserver.js
@@ -1,10 +1,16 @@
 
-var express = require('express')
+var eventEmitter = require('events'),
+    evt = new eventEmitter,
+	express = require('express')
    , nano    = require('nano')('http://localhost:5984')
    , actions = nano.use("actions")
    , requests = nano.use("requests")
    , app     = express()
    ;
+
+var maxTime = 2000; // time in ms before timing out sync requests
+var changeInterval = 200; // interval in ms to look for changes
+var lastSeq = 0;
    
 // async request, return the requestID so user can use /requests/:requestid to pickup the result
 function handleAsync(res){
@@ -18,29 +24,54 @@ function handleAsync(res){
 	}
 }
 
-function returnResult(res,id){
+function returnSyncResult(res,id,type){
+	if (type == "timeout"){
+		res.status(504); // timeout
+		return res.send("request timed out");
+	}
 	requests.get(id, function(err,result){
-		if (result.status == "processing"){
-			res.status(504); // timeout
-			return res.send("request timed out");
-		}
+		// return the result
 		res.status(200);
 		res.send(result);
 	});
 }
 
-// TODO: for synchronous requests we need to return the result of the action, for now we cheat by waiting 500ms ;-)
-// better solution is to watch changes on the DB
+// for synchronous requests we need to return the result of the action
 function handleSync(res){
 	return function(error, body) {
+		var timer;
 		if (error) {
 			res.status(error.statusCode);
 			return res.send(error.message);
 		}
-		setTimeout( function(){
-			returnResult(res,body.id);
-		},500);
+		// wait for an event to pickup the result from the queue and return it to the requester
+		evt.once(body.id, function(type){
+			returnSyncResult(res,body.id,type);
+		});
+		// if it takes longer than maxTime, return a timeout
+		setTimeout(function(){ 
+			evt.emit(body.id,"timeout");
+			},
+		maxTime
+		);
 	}
+}
+
+// pouchDB seems to have a problem with streaming changes and with filtering changes, so we poll on a regular basis and filter here
+function handleRequestsChanges(){
+	requests.changes( { since:lastSeq, include_docs:true },
+		function (err,body){
+			if ( lastSeq != body.last_seq){
+				body.results.forEach(function (item){
+					var status = item.doc.status;
+					if (( status != "new" ) && (status != "processing")){
+						evt.emit(item.id);
+					}
+				});
+			}
+			lastSeq = body.last_seq;
+		}
+	);
 }
 
 app.get('/actions', function (req, res) {
@@ -83,6 +114,8 @@ app.get('/*', function (req, res) {
 		requests.insert(record,handler);
 	});	
 });
+
+setInterval(handleRequestsChanges,changeInterval);
 
 app.listen(3000, function () {
   console.log('Api server listening on port 3000!');
