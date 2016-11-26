20055f8e744b956c5601b2b93e120d0d080c7abc
seriousme <hans.klunder@xs4all.nl>
2016-11-26 16:53:00 +0100

    Upgraded to Vue 2.x

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,14 @@ Vue.filter('encodeURI', function(data) {
     return (encodeURIComponent(data));
 });
 
+var appData = {
+    action: {},
+    request: {},
+    actions: [],
+    requests: [],
+    page: "",
+    hash: ""
+};
 var app = new Vue({
 
     // We want to target the div with an id of 'events'
@@ -17,50 +25,36 @@ var app = new Vue({
 
     // Here we can register any values or collections that hold data
     // for the application
-    data: {
-        action: {},
-        request: {},
-        actions: [],
-        requests: [],
-        page: "",
-        hash: ""
-    },
-
-    // Anything within the ready function will run when the application loads
-    ready: function() {
-        // When the application loads, we want to call the method that initializes
-        // some data
-        //this.fetchData();
-    },
+    data: appData,
 
     // Methods we want to use in our application are registered here
     methods: {
 
         // We dedicate a method to retrieving and setting some data
         newAction: function() {
-            this.$set('action', {
+            appData.action = {
                 _id: "",
                 code: ""
-            });
-            this.$set('page', 'showAction');
+            };
+            appData.page = 'showAction';
         },
         newRequest: function() {
             this.fetchActions(this.newRequestStage2);
         },
         newRequestStage2: function(actions) {
-            this.$set('request', {
+            appData.request = {
                 'path': actions[0]._id,
                 'params': "",
                 'response': ""
-            });
-            this.$set('page', 'newRequest');
+            };
+            appData.page = 'newRequest';
         },
         showAction: function(ctx) {
             var actionid = ctx.params[0];
             this.$http.get(baseUrl + "actions/" + encodeURIComponent(actionid)).then((response) => {
                 // success callback
-                this.$set('action', response.json());
-                this.$set('page', 'showAction');
+                appData.action = response.body;
+                appData.page = 'showAction';
             }, (response) => {
                 doNotify("danger", "Failed to load request");
             });
@@ -70,8 +64,8 @@ var app = new Vue({
           var requestid = ctx.params.requestid;
             this.$http.get(baseUrl + "requests/" + encodeURIComponent(requestid)).then((response) => {
                 // success callback
-                this.$set('request', response.json());
-                this.$set('page', 'showRequest');
+                appData.request =  response.body;
+                appData.page = 'showRequest';
             }, (response) => {
                 doNotify("danger", "Failed to load request");
             });
@@ -79,22 +73,22 @@ var app = new Vue({
         },
         listActions: function() {
             this.fetchActions();
-            this.$set('hash', 'actions');
-            this.$set('page', 'listActions');
+            appData.hash = 'actions';
+            appData.page = 'listActions';
         },
         listRequests: function() {
             this.fetchRequests();
-            this.$set('hash', 'requests');
-            this.$set('page', 'listRequests');
+            appData.hash = 'requests';
+            appData.page = 'listRequests';
         },
         fetchActions: function(callback) {
             this.$http.get(baseUrl + "actions").then((response) => {
                 // success callback
                 var actions = [];
-                response.json().rows.forEach(function(row) {
+                response.body.rows.forEach(function(row) {
                     actions.push(row.value);
                 });
-                this.$set('actions', actions);
+                appData.actions = actions;
                 if (callback) {
                     callback(actions);
                 }
@@ -107,10 +101,10 @@ var app = new Vue({
             this.$http.get(baseUrl + "requests").then((response) => {
                 // success callback
                 var requests = [];
-                response.json().rows.forEach(function(row) {
+                response.body.rows.forEach(function(row) {
                     requests.push(row.value);
                 });
-                this.$set('requests', requests);
+                appData.requests = requests;
 
 
             }, (response) => {
@@ -130,7 +124,7 @@ var app = new Vue({
                     body._rev = action._rev;
                     this.$http.put(baseUrl + "actions/" + encodeURIComponent(action._id), body).then((response) => {
                         // success callback
-                        doNotify('info', 'Succesfully updated action: ' + response.json().id);
+                        doNotify('info', 'Succesfully updated action: ' + response.body.id);
                         page.show('/actions');
                     }, (response) => {
                         // error callback
@@ -140,7 +134,7 @@ var app = new Vue({
                 } else {
                     this.$http.put(baseUrl + "actions/" + encodeURIComponent(action._id), body).then((response) => {
                         // success callback
-                        doNotify('info', 'Succesfully updated action: ' + response.json().id);
+                        doNotify('info', 'Succesfully updated action: ' + response.body.id);
                         page.show('/actions');
                     }, (response) => {
                         var errTxt = "";
@@ -160,7 +154,7 @@ var app = new Vue({
             this.$http.get(request.path, {
                 'params': JSON.parse(request.params)
             }).then((response) => {
-                doNotify('info', 'Succesfully created request with ID: ' + response.json().id);
+                doNotify('info', 'Succesfully created request with ID: ' + response.body.id);
             }, (response) => {
                 // error callback
                 doNotify("danger", "Failed to create new request");
@@ -172,13 +166,13 @@ var app = new Vue({
                 this.$http.delete(baseUrl + "actions/" + encodeURIComponent(action._id),{
                   '_rev': action._rev
                 }).then((response) => {
-                    doNotify('info', 'Succesfully deleted action with ID: ' + response.json().id);
+                    doNotify('info', 'Succesfully deleted action with ID: ' + response.body.id);
                 }, (response) => {
                     // error callback
                     doNotify("danger", "Failed to create new request");
                 });
-                // $remove is a Vue convenience method similar to splice
-                this.actions.$remove(action);
+                var index = this.actions.indexOf(action);
+                this.actions.splice(index, 1);
                 page.show('/actions');
             }
         },
