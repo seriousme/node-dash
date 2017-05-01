92bbc87b5843e7154e1cb20f1ab2698fcb633d0a
Hans Klunder <hans.klunder@xs4all.nl>
2017-05-01 08:32:19 +0200

    Reworked database loading

--- createDB.js
+++ createDB.js
@@ -1,46 +1,74 @@
 var DbUrl = process.env.DB_URL || 'http://localhost:5984'
 const PouchDB = require('pouchdb-node')
-
-var http = require('http')
+const designDocs = require('./designDocs.json')
 
 const maxTries = 10
-const tries = {}
+var tries = {}
 const retryMs = 500
 
 const now = Date.now()
 
-// Handling socket errors in Nano is not trivial,
-// cheating here by doing a http request first,
-// if that works we'll start the show
-
-function bulkInsert (dbname, data) {
-  if (typeof (tries[dbname]) === 'undefined') {
-    tries[dbname] = 0
+// demoData, make the timestamps look like they are recent
+const demoData = {
+  'actions': [{
+    '_id': '/myactions/sum',
+    'code': 'function main(params){ return { "sum": Number(params.a) + Number(params.b)};}'
+  }, {
+    '_id': '/myactions/mult',
+    'code': 'function main(params){ return { "mult":Number(params.a) * Number(params.b)};}'
   }
-  console.log('Attempt', tries[dbname], 'of', maxTries, 'to connect to ', dbname, 'at', DbUrl)
-  http.get(DbUrl, (res) => {
-    console.log('Database server at', DbUrl, 'is up !')
-    // consume response body
-    res.resume()
-    doInsert(dbname, data)
-  }).on('error', (e) => {
-    // console.log(`Got error: ${e.message}`)
-    if (tries[dbname]++ < maxTries) {
-      console.log('Going to retry in', retryMs, 'ms')
-      setTimeout(() => { bulkInsert(dbname, data) }, retryMs)
-    } else {
-      console.log('Failed to connect to', dbname, 'at', DbUrl)
+  ],
+  'requests': [
+    {
+      'path': '/myactions/sum',
+      'timestamp': (new Date((now - 8000000))).toISOString(),
+      'params': {
+        'a': 1,
+        'b': 2
+      },
+      'result': {
+        'sum': '3'
+      },
+      'status': 'success'
+    },
+    {
+      'path': '/myactions/sum',
+      'timestamp': (new Date((now - 320000))).toISOString(),
+      'params': {
+        'a': 1,
+        'b': 2
+      },
+      'status': 'new'
+    },
+    {
+      'path': '/myactions/mult',
+      'timestamp': (new Date((now - 140000))).toISOString(),
+      'params': {
+        'a': 1,
+        'b': 2
+      },
+      'status': 'new'
     }
-  })
+  ]
 }
 
+// try to empty the DB, if the DB is not present try again after some time
 function destroyDB (dbname, callback) {
   const db = new PouchDB(`${DbUrl}/${dbname}`)
   db.destroy(function (err, _) {
     if (err) {
-      return console.log('destroying database', dbname, 'failed:', err)
+      if (typeof (tries[dbname]) === 'undefined') {
+        tries[dbname] = 0
+      }
+      if (tries[dbname]++ < maxTries) {
+        console.log('Going to retry to connect to', dbname, 'in', retryMs, 'ms')
+        setTimeout(() => { destroyDB(dbname, callback) }, retryMs)
+      } else {
+        console.log('Failed to connect to database at', DbUrl)
+      }
+    } else {
+      callback()
     }
-    callback()
   })
 }
 
@@ -58,67 +86,18 @@ function doInsert (dbname, data) {
   })
 }
 
-bulkInsert('actions', [{
-  '_id': '/myactions/sum',
-  'code': 'function main(params){ return { "sum": Number(params.a) + Number(params.b)};}'
-}, {
-  '_id': '/myactions/mult',
-  'code': 'function main(params){ return { "mult":Number(params.a) * Number(params.b)};}'
-},
-{
-  'views': {
-    'all': {
-      'map': "function (doc) {\n    emit(null,{'_id':doc._id,'_rev':doc._rev});\n}\n"
-    }
-  },
-  '_id': '_design/actions'
-}
-])
+// start the show
 
-bulkInsert('requests', [
-  {
-    'path': '/myactions/sum',
-    'timestamp': (new Date((now - 8000000))).toISOString(),
-    'params': {
-      'a': 1,
-      'b': 2
-    },
-    'result': {
-      'sum': '3'
-    },
-    'status': 'success'
-  },
-  {
-    'path': '/myactions/sum',
-    'timestamp': (new Date((now - 320000))).toISOString(),
-    'params': {
-      'a': 1,
-      'b': 2
-    },
-    'status': 'new'
-  },
-  {
-    'path': '/myactions/mult',
-    'timestamp': (new Date((now - 140000))).toISOString(),
-    'params': {
-      'a': 1,
-      'b': 2
-    },
-    'status': 'new'
-  },
-  {
-    'views': {
-      'all': {
-        'map': "function (doc) {\n  emit(doc.timestamp,{'id':doc._id, 'path':doc.path, 'status':doc.status,'timestamp':doc.timestamp});\n}"
-      },
-      'new': {
-        'map': 'function (doc) {\n if (doc.status == "new"){\n  emit(doc._id, 1);\n }\n}'
-      }
-    },
-    'filters': {
-      'iscompleted': 'function (doc) {\n return ((doc.status === "success") || (doc.status === "failed"));}',
-      'isnew': 'function (doc) {\n return (doc.status === "new");}'
-    },
-    '_id': '_design/requests'
+// merge the examples with the design docs
+for (let dbname in designDocs) {
+  if (typeof (demoData[dbname]) === 'undefined') {
+    demoData[dbname] = []
   }
-])
+  for (let item in designDocs[dbname]) {
+    demoData[dbname].push(designDocs[dbname][item])
+  }
+}
+
+for (let dbname in demoData) {
+  doInsert(dbname, demoData[dbname])
+}
