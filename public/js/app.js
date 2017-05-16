5bba78cfa587c55a688b517d15238643d58fbf27
Hans Klunder <hans.klunder@xs4all.nl>
2017-05-16 12:13:08 +0200

    Babelified app.js to make it run in IE11

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,16 @@
+'use strict';
+
 // app.js
 
-var baseUrl = '/dash/'
+var baseUrl = '/dash/';
 
 Vue.filter('since', function (timestamp) {
-  return (dateSince(Date.parse(timestamp)) + ' ago')
-})
+  return dateSince(Date.parse(timestamp)) + ' ago';
+});
 
 Vue.filter('encodeURI', function (data) {
-  return (encodeURIComponent(data))
-})
+  return encodeURIComponent(data);
+});
 
 var appData = {
   action: {},
@@ -17,7 +19,7 @@ var appData = {
   requests: [],
   page: '',
   hash: ''
-}
+};
 var app = new Vue({
 
   // We want to target the div with an id of 'events'
@@ -31,175 +33,178 @@ var app = new Vue({
   methods: {
 
     // We dedicate a method to retrieving and setting some data
-    newAction: function () {
+    newAction: function newAction() {
       appData.action = {
         _id: '',
         code: ''
-      }
-      appData.page = 'showAction'
+      };
+      appData.page = 'showAction';
     },
-    newRequest: function () {
-      var uri = baseUrl + 'actions'
-      this.fetchItems('actions', uri, (items) => {
-        appData.actions = items
+    newRequest: function newRequest() {
+      var uri = baseUrl + 'actions';
+      this.fetchItems('actions', uri, function (items) {
+        appData.actions = items;
         appData.request = {
           'path': items[0]._id,
           'params': '{ "a": 5, "b": 2 }',
           'response': ''
-        }
-        appData.page = 'newRequest'
-      })
+        };
+        appData.page = 'newRequest';
+      });
     },
-    showItem: function (name, uri, callback) {
-      this.$http.get(uri).then((response) => {
+    showItem: function showItem(name, uri, callback) {
+      this.$http.get(uri).then(function (response) {
         // success callback
         if (callback) {
-          callback(response.body)
+          callback(response.body);
         }
-      }, (response) => {
-        doNotify('danger', 'Failed to load ' + name)
-      })
+      }, function (response) {
+        doNotify('danger', 'Failed to load ' + name);
+      });
     },
-    showAction: function (ctx) {
-      var uri = baseUrl + 'actions/' + encodeURIComponent(ctx.params[0])
-      this.showItem('action', uri, (item) => {
-        appData.action = item
-        appData.page = 'showAction'
-      })
+    showAction: function showAction(ctx) {
+      var uri = baseUrl + 'actions/' + encodeURIComponent(ctx.params[0]);
+      this.showItem('action', uri, function (item) {
+        appData.action = item;
+        appData.page = 'showAction';
+      });
     },
-    showRequest: function (ctx) {
-      var uri = baseUrl + 'requests/' + encodeURIComponent(ctx.params.requestid)
-      this.showItem('request', uri, (item) => {
-        appData.request = item
-        appData.page = 'showRequest'
-      })
+    showRequest: function showRequest(ctx) {
+      var uri = baseUrl + 'requests/' + encodeURIComponent(ctx.params.requestid);
+      this.showItem('request', uri, function (item) {
+        appData.request = item;
+        appData.page = 'showRequest';
+      });
     },
-    fetchItems (name, uri, callback) {
-      this.$http.get(uri).then((response) => {
+    fetchItems: function fetchItems(name, uri, callback) {
+      this.$http.get(uri).then(function (response) {
         // success callback
-        var items = []
+        var items = [];
         response.body.rows.forEach(function (row) {
-          items.push(row.value)
-        })
+          items.push(row.value);
+        });
         if (callback) {
-          callback(items)
+          callback(items);
         }
-      }, (response) => {
+      }, function (response) {
         // error callback
-        doNotify('danger', 'Failed to load ' + name)
-      })
+        doNotify('danger', 'Failed to load ' + name);
+      });
     },
-    listActions: function () {
-      var uri = baseUrl + 'actions'
-      this.fetchItems('actions', uri, (items) => {
-        appData.actions = items
-        appData.hash = 'actions'
-        appData.page = 'listActions'
-      })
+
+    listActions: function listActions() {
+      var uri = baseUrl + 'actions';
+      this.fetchItems('actions', uri, function (items) {
+        appData.actions = items;
+        appData.hash = 'actions';
+        appData.page = 'listActions';
+      });
     },
-    listRequests: function () {
-      var uri = baseUrl + 'requests'
-      this.fetchItems('requests', uri, (items) => {
-        appData.requests = items
-        appData.hash = 'requests'
-        appData.page = 'listRequests'
-      })
+    listRequests: function listRequests() {
+      var uri = baseUrl + 'requests';
+      this.fetchItems('requests', uri, function (items) {
+        appData.requests = items;
+        appData.hash = 'requests';
+        appData.page = 'listRequests';
+      });
     },
-    saveAction: function (action) {
+    saveAction: function saveAction(action) {
       // console.log(this.action)
 
-      if ((action._id) && (action.code)) {
+      if (action._id && action.code) {
         var body = {
           '_id': action._id,
           'code': action.code
-        }
-        var txt = 'created'
+        };
+        var txt = 'created';
         if (action._rev) {
-          body._rev = action._rev
-          txt = 'updated'
+          body._rev = action._rev;
+          txt = 'updated';
         }
-        this.$http.put(baseUrl + 'actions/' + encodeURIComponent(action._id), body).then((response) => {
+        this.$http.put(baseUrl + 'actions/' + encodeURIComponent(action._id), body).then(function (response) {
           // success callback
-          doNotify('info', 'Succesfully ' + txt + ' action: ' + response.body.id)
-          page.show('/actions')
-        }, (response) => {
+          doNotify('info', 'Succesfully ' + txt + ' action: ' + response.body.id);
+          page.show('/actions');
+        }, function (response) {
           // error callback
-          var errTxt = ''
+          var errTxt = '';
           if (response.status === 409) {
-            errTxt = ': action with this path already exists'
+            errTxt = ': action with this path already exists';
           }
-          doNotify('danger', 'Failed to save action' + errTxt)
-        })
+          doNotify('danger', 'Failed to save action' + errTxt);
+        });
       } else {
-        doNotify('danger', 'Path and Code need to be entered')
+        doNotify('danger', 'Path and Code need to be entered');
       }
     },
-    saveRequest: function (request) {
+    saveRequest: function saveRequest(request) {
       this.$http.get(request.path, {
         'params': JSON.parse(request.params)
-      }).then((response) => {
-        doNotify('info', 'Succesfully created request with ID: ' + response.body.id)
-      }, (response) => {
+      }).then(function (response) {
+        doNotify('info', 'Succesfully created request with ID: ' + response.body.id);
+      }, function (response) {
         // error callback
-        doNotify('danger', 'Failed to create new request')
-      })
+        doNotify('danger', 'Failed to create new request');
+      });
     },
 
-    deleteAction: function (action) {
+    deleteAction: function deleteAction(action) {
+      var _this = this;
+
       if (confirm('Are you sure you want to delete action ' + action._id + ' ?')) {
         this.$http.delete(baseUrl + 'actions/' + encodeURIComponent(action._id), {
           '_rev': action._rev
-        }).then((response) => {
-          doNotify('info', 'Succesfully deleted action with ID: ' + response.body.id)
-          this.fetchActions()
-        }, (response) => {
+        }).then(function (response) {
+          doNotify('info', 'Succesfully deleted action with ID: ' + response.body.id);
+          _this.fetchActions();
+        }, function (response) {
           // error callback
-          doNotify('danger', 'Failed to create new request')
-        })
-        page.show('/actions')
+          doNotify('danger', 'Failed to create new request');
+        });
+        page.show('/actions');
       }
     },
-    exists: function (v) {
-      return (typeof (v) !== 'undefined')
+    exists: function exists(v) {
+      return typeof v !== 'undefined';
     },
-    setPage: function (path, v) {
-      if (typeof (v) === 'string') {
-        path = path + encodeURIComponent(v)
+    setPage: function setPage(path, v) {
+      if (typeof v === 'string') {
+        path = path + encodeURIComponent(v);
       }
-      console.log('setPage', path)
-      page.show(path)
+      console.log('setPage', path);
+      page.show(path);
     }
   }
-})
+});
 
-function dateSince (date) {
-  var seconds = Math.floor((new Date() - date) / 1000)
+function dateSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
 
-  var interval = Math.floor(seconds / 31536000)
+  var interval = Math.floor(seconds / 31536000);
 
   if (interval > 1) {
-    return interval + ' years'
+    return interval + ' years';
   }
-  interval = Math.floor(seconds / 2592000)
+  interval = Math.floor(seconds / 2592000);
   if (interval > 1) {
-    return interval + ' months'
+    return interval + ' months';
   }
-  interval = Math.floor(seconds / 86400)
+  interval = Math.floor(seconds / 86400);
   if (interval > 1) {
-    return interval + ' days'
+    return interval + ' days';
   }
-  interval = Math.floor(seconds / 3600)
+  interval = Math.floor(seconds / 3600);
   if (interval > 1) {
-    return interval + ' hours'
+    return interval + ' hours';
   }
-  interval = Math.floor(seconds / 60)
+  interval = Math.floor(seconds / 60);
   if (interval > 1) {
-    return interval + ' minutes'
+    return interval + ' minutes';
   }
-  return Math.floor(seconds) + ' seconds'
+  return Math.floor(seconds) + ' seconds';
 }
 
-function doNotify (type, txt) {
+function doNotify(type, txt) {
   $.notify({
     // options
     message: txt
@@ -213,15 +218,15 @@ function doNotify (type, txt) {
       from: 'top',
       align: 'center'
     }
-  })
+  });
 }
 
-page.base(baseUrl + 'ui')
-page('/requests', app.listRequests)
-page('/requests/new', app.newRequest)
-page('/requests/:requestid', app.showRequest)
-page('/actions', app.listActions)
-page('/actions/new', app.newAction)
-page('/actions/*', app.showAction)
-page('/', '/requests')
-page.start()
+page.base(baseUrl + 'ui');
+page('/requests', app.listRequests);
+page('/requests/new', app.newRequest);
+page('/requests/:requestid', app.showRequest);
+page('/actions', app.listActions);
+page('/actions/new', app.newAction);
+page('/actions/*', app.showAction);
+page('/', '/requests');
+page.start();
