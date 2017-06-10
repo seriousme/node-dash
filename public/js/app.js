5b905b65aa766f9e32e3d087e185f79db310b076
Hans Klunder <hans.klunder@xs4all.nl>
2017-06-10 10:42:55 +0200

    Using "Prettier" formatting

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 // app.js
 
-var baseUrl = '/dash/';
+var baseUrl = "/dash/";
 
-Vue.filter('since', function (timestamp) {
-  return dateSince(Date.parse(timestamp)) + ' ago';
+Vue.filter("since", function(timestamp) {
+  return dateSince(Date.parse(timestamp)) + " ago";
 });
 
-Vue.filter('encodeURI', function (data) {
+Vue.filter("encodeURI", function(data) {
   return encodeURIComponent(data);
 });
 
@@ -17,13 +17,12 @@ var appData = {
   request: {},
   actions: [],
   requests: [],
-  page: '',
-  hash: ''
+  page: "",
+  hash: ""
 };
 var app = new Vue({
-
   // We want to target the div with an id of 'events'
-  el: '#dash',
+  el: "#dash",
 
   // Here we can register any values or collections that hold data
   // for the application
@@ -31,81 +30,87 @@ var app = new Vue({
 
   // Methods we want to use in our application are registered here
   methods: {
-
     // We dedicate a method to retrieving and setting some data
     newAction: function newAction() {
       appData.action = {
-        _id: '',
-        code: ''
+        _id: "",
+        code: ""
       };
-      appData.page = 'showAction';
+      appData.page = "showAction";
     },
     newRequest: function newRequest() {
-      var uri = baseUrl + 'actions';
-      this.fetchItems('actions', uri, function (items) {
+      var uri = baseUrl + "actions";
+      this.fetchItems("actions", uri, function(items) {
         appData.actions = items;
         appData.request = {
-          'path': items[0]._id,
-          'params': '{ "a": 5, "b": 2 }',
-          'response': ''
+          path: items[0]._id,
+          params: '{ "a": 5, "b": 2 }',
+          response: ""
         };
-        appData.page = 'newRequest';
+        appData.page = "newRequest";
       });
     },
     showItem: function showItem(name, uri, callback) {
-      this.$http.get(uri).then(function (response) {
-        // success callback
-        if (callback) {
-          callback(response.body);
+      this.$http.get(uri).then(
+        function(response) {
+          // success callback
+          if (callback) {
+            callback(response.body);
+          }
+        },
+        function(response) {
+          doNotify("danger", "Failed to load " + name);
         }
-      }, function (response) {
-        doNotify('danger', 'Failed to load ' + name);
-      });
+      );
     },
     showAction: function showAction(ctx) {
-      var uri = baseUrl + 'actions/' + encodeURIComponent(ctx.params[0]);
-      this.showItem('action', uri, function (item) {
+      var uri = baseUrl + "actions/" + encodeURIComponent(ctx.params[0]);
+      this.showItem("action", uri, function(item) {
         appData.action = item;
-        appData.page = 'showAction';
+        appData.page = "showAction";
       });
     },
     showRequest: function showRequest(ctx) {
-      var uri = baseUrl + 'requests/' + encodeURIComponent(ctx.params.requestid);
-      this.showItem('request', uri, function (item) {
+      var uri =
+        baseUrl + "requests/" + encodeURIComponent(ctx.params.requestid);
+      this.showItem("request", uri, function(item) {
         appData.request = item;
-        appData.page = 'showRequest';
+        appData.page = "showRequest";
       });
     },
     fetchItems: function fetchItems(name, uri, callback) {
-      this.$http.get(uri).then(function (response) {
-        // success callback
-        var items = [];
-        response.body.rows.forEach(function (row) {
-          items.push(row.value);
-        });
-        if (callback) {
-          callback(items);
+      this.$http.get(uri).then(
+        function(response) {
+          // success callback
+          var items = [];
+          response.body.rows.forEach(function(row) {
+            items.push(row.value);
+          });
+          if (callback) {
+            callback(items);
+          }
+        },
+        function(response) {
+          // error callback
+          doNotify("danger", "Failed to load " + name);
         }
-      }, function (response) {
-        // error callback
-        doNotify('danger', 'Failed to load ' + name);
-      });
+      );
     },
 
     listActions: function listActions() {
-      var uri = baseUrl + 'actions';
-      this.fetchItems('actions', uri, function (items) {
+      var uri = baseUrl + "actions";
+      this.fetchItems("actions", uri, function(items) {
         appData.actions = items;
-        appData.hash = 'actions';
-        appData.page = 'listActions';
+        appData.hash = "actions";
+        appData.page = "listActions";
       });
     },
     listRequests: function listRequests() {
-      var uri = baseUrl + 'requests';
-      this.fetchItems('requests', uri, function (items) {
+      var uri = baseUrl + "requests";
+      this.fetchItems("requests", uri, function(items) {
         appData.requests = items;
-        appData.hash = 'requests';
-        appData.page = 'listRequests';
+        appData.hash = "requests";
+        appData.page = "listRequests";
       });
     },
     saveAction: function saveAction(action) {
@@ -113,66 +118,92 @@ var app = new Vue({
 
       if (action._id && action.code) {
         var body = {
-          '_id': action._id,
-          'code': action.code
+          _id: action._id,
+          code: action.code
         };
-        var txt = 'created';
+        var txt = "created";
         if (action._rev) {
           body._rev = action._rev;
-          txt = 'updated';
+          txt = "updated";
         }
-        this.$http.put(baseUrl + 'actions/' + encodeURIComponent(action._id), body).then(function (response) {
-          // success callback
-          doNotify('info', 'Succesfully ' + txt + ' action: ' + response.body.id);
-          page.show('/actions');
-        }, function (response) {
-          // error callback
-          var errTxt = '';
-          if (response.status === 409) {
-            errTxt = ': action with this path already exists';
-          }
-          doNotify('danger', 'Failed to save action' + errTxt);
-        });
+        this.$http
+          .put(baseUrl + "actions/" + encodeURIComponent(action._id), body)
+          .then(
+            function(response) {
+              // success callback
+              doNotify(
+                "info",
+                "Succesfully " + txt + " action: " + response.body.id
+              );
+              page.show("/actions");
+            },
+            function(response) {
+              // error callback
+              var errTxt = "";
+              if (response.status === 409) {
+                errTxt = ": action with this path already exists";
+              }
+              doNotify("danger", "Failed to save action" + errTxt);
+            }
+          );
       } else {
-        doNotify('danger', 'Path and Code need to be entered');
+        doNotify("danger", "Path and Code need to be entered");
       }
     },
     saveRequest: function saveRequest(request) {
-      this.$http.get(request.path, {
-        'params': JSON.parse(request.params)
-      }).then(function (response) {
-        doNotify('info', 'Succesfully created request with ID: ' + response.body.id);
-        page.show('/requests');
-      }, function (response) {
-        // error callback
-        doNotify('danger', 'Failed to create new request');
-      });
+      this.$http
+        .get(request.path, {
+          params: JSON.parse(request.params)
+        })
+        .then(
+          function(response) {
+            doNotify(
+              "info",
+              "Succesfully created request with ID: " + response.body.id
+            );
+            page.show("/requests");
+          },
+          function(response) {
+            // error callback
+            doNotify("danger", "Failed to create new request");
+          }
+        );
     },
 
     deleteAction: function deleteAction(action) {
       var _this = this;
 
-      if (confirm('Are you sure you want to delete action ' + action._id + ' ?')) {
-        this.$http.delete(baseUrl + 'actions/' + encodeURIComponent(action._id), {
-          '_rev': action._rev
-        }).then(function (response) {
-          doNotify('info', 'Succesfully deleted action with ID: ' + response.body.id);
-          _this.fetchActions();
-        }, function (response) {
-          // error callback
-          doNotify('danger', 'Failed to create new request');
-        });
-        page.show('/actions');
+      if (
+        confirm("Are you sure you want to delete action " + action._id + " ?")
+      ) {
+        this.$http
+          .delete(baseUrl + "actions/" + encodeURIComponent(action._id), {
+            _rev: action._rev
+          })
+          .then(
+            function(response) {
+              doNotify(
+                "info",
+                "Succesfully deleted action with ID: " + response.body.id
+              );
+              _this.fetchActions();
+            },
+            function(response) {
+              // error callback
+              doNotify("danger", "Failed to create new request");
+            }
+          );
+        page.show("/actions");
       }
     },
     exists: function exists(v) {
-      return typeof v !== 'undefined';
+      return typeof v !== "undefined";
     },
     setPage: function setPage(path, v) {
-      if (typeof v === 'string') {
+      if (typeof v === "string") {
         path = path + encodeURIComponent(v);
       }
-      console.log('setPage', path);
+      console.log("setPage", path);
       page.show(path);
     }
   }
@@ -184,50 +215,53 @@ function dateSince(date) {
   var interval = Math.floor(seconds / 31536000);
 
   if (interval > 1) {
-    return interval + ' years';
+    return interval + " years";
   }
   interval = Math.floor(seconds / 2592000);
   if (interval > 1) {
-    return interval + ' months';
+    return interval + " months";
   }
   interval = Math.floor(seconds / 86400);
   if (interval > 1) {
-    return interval + ' days';
+    return interval + " days";
   }
   interval = Math.floor(seconds / 3600);
   if (interval > 1) {
-    return interval + ' hours';
+    return interval + " hours";
   }
   interval = Math.floor(seconds / 60);
   if (interval > 1) {
-    return interval + ' minutes';
+    return interval + " minutes";
   }
-  return Math.floor(seconds) + ' seconds';
+  return Math.floor(seconds) + " seconds";
 }
 
 function doNotify(type, txt) {
-  $.notify({
-    // options
-    message: txt
-  }, {
-    // settings
-    element: 'body',
-    type: type,
-    mouse_over: 'pause',
-    delay: 3000,
-    placement: {
-      from: 'top',
-      align: 'center'
+  $.notify(
+    {
+      // options
+      message: txt
+    },
+    {
+      // settings
+      element: "body",
+      type: type,
+      mouse_over: "pause",
+      delay: 3000,
+      placement: {
+        from: "top",
+        align: "center"
+      }
     }
-  });
+  );
 }
 
-page.base(baseUrl + 'ui');
-page('/requests', app.listRequests);
-page('/requests/new', app.newRequest);
-page('/requests/:requestid', app.showRequest);
-page('/actions', app.listActions);
-page('/actions/new', app.newAction);
-page('/actions/*', app.showAction);
-page('/', '/requests');
+page.base(baseUrl + "ui");
+page("/requests", app.listRequests);
+page("/requests/new", app.newRequest);
+page("/requests/:requestid", app.showRequest);
+page("/actions", app.listActions);
+page("/actions/new", app.newAction);
+page("/actions/*", app.showAction);
+page("/", "/requests");
 page.start();
